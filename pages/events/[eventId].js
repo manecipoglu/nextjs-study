1c12055f4552d243a2269bc8f034e931368adb16
manecipoglu <muammernecipoglu@gmail.com>
2021-07-08 13:22:09 -0700

    change setup for the events website

---
+++ pages/events/[eventId].js
@@ -0,0 +1,7 @@
+export default function EventDetails() {
+  return (
+    <div>
+      <h1>Event Detail Page</h1>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,7 @@
-import Link from "next/link";
-
 export default function HomePage() {
   return (
     <div>
-      <h1>HomePage</h1>
-      <ul>
-        <li>
-          <Link href="/portfolio">Portfolio Page</Link>
-        </li>
-        <li>
-          <Link href="/blog">Blog Page</Link>
-        </li>
-      </ul>
+      <h1>Home Page</h1>
     </div>
   );
 }

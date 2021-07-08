10eaa3f02f06901838c0fcb436cc7d2867c688cb
manecipoglu <muammernecipoglu@gmail.com>
2021-07-07 22:20:46 -0700

    use Link to naviage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
     <div>
       <h1>HomePage</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio Page</Link>
+        </li>
+        <li>
+          <Link href="/blog">Blog Page</Link>
+        </li>
+      </ul>
     </div>
   );
 }

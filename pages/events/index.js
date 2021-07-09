d2adf2a04c07a73f88373e38d10c57bca16480a8
manecipoglu <muammernecipoglu@gmail.com>
2021-07-09 13:01:20 -0700

    add event-search component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,13 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/event-list/EventList";
+import EventSearch from "../../components/event-search/EventSearch";
 
 export default function Events() {
   const events = getAllEvents();
   return (
-    <div>
+    <>
+      <EventSearch />
       <EventList events={events} />
-    </div>
+    </>
   );
 }

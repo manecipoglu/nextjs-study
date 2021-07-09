6882a559b477f9ba3167828716760086b7a74a0b
manecipoglu <muammernecipoglu@gmail.com>
2021-07-08 21:04:48 -0700

    add EventItem and EventList components

---
+++ components/EventList.jsx
@@ -0,0 +1,5 @@
+import EventItem from "./EventItem";
+
+export default function EventList(props) {
+  return props.events.map(event => <EventItem key={event.id}></EventItem>);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import { getFeaturedEvents } from "../dummy-data";
+
 export default function HomePage() {
+  const featuredEvents = getFeaturedEvents();
   return (
     <div>
-      <h1>Home Page</h1>
+      {featuredEvents.map(event => (
+        <h1 key={event.id}>{event.title}</h1>
+      ))}
     </div>
   );
 }

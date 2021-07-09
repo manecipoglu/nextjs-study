acac7f280bbc20597c521a76211ff06dcea12b0b
manecipoglu <muammernecipoglu@gmail.com>
2021-07-09 11:51:58 -0700

    add AllEvents page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,11 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/event-list/EventList";
+
 export default function Events() {
+  const events = getAllEvents();
   return (
     <div>
-      <h1>Events Page</h1>
+      <EventList events={events} />
     </div>
   );
 }

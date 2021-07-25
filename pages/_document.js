516aa898b29c682d82bab3d18ff676a6276e564c
manecipoglu <muammernecipoglu@gmail.com>
2021-07-24 22:50:55 -0700

    configure _document.js

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          {/* having a div outside NextJs components allows
+        us to use things like React Portal to summon our Modals etc. */}
+          <div id="overlays" />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}

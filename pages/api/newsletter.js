844c899bdcc41c248782fddda096c43e12acae7e
manecipoglu <muammernecipoglu@gmail.com>
2021-08-10 17:31:23 -0700

    establish connection to mongodb database

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,7 +1,17 @@
-export default function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
   if (req.method === "POST") {
-    const email = req.body.email;
-    console.log(`newsletter subscribed to ${email}`);
+    const userEmail = req.body.email;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://admin:admin@cluster0.1txxj.mongodb.net/newsletter?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    await db.collection("emails").insertOne({ email: userEmail });
+
+    client.close();
+
+    res.status(201).json({ message: "email registered!" });
   }
-  res.status(200).json({ message: "this works!" });
 }
